1db862c67dd709f10ff0bef47e6bceacec7135f3
GregoriSimei <gregorisimei@gmail.com>
2025-03-05 13:54:50 -0300

    feat: receive sqs event

--- src/payment-lambda.ts
+++ src/payment-lambda.ts
@@ -1,13 +1,15 @@
-import { APIGatewayEvent, Context } from "aws-lambda";
+import { Context, SQSEvent } from "aws-lambda";
 
 export class PaymentLambda {
-    static async handler(event: APIGatewayEvent, context: Context) {
+    static async handler(event: SQSEvent, context: Context) {
+        const record = event.Records[0];
+        const body = JSON.parse(record.body);
         console.log('PaymentLambda.handler', event);
         return {
             statusCode: 200,
             body: JSON.stringify({
                 message: 'PaymentLambda.handler executed successfully!',
-                input: event,
+                input: body,
             }),
         };
     }
