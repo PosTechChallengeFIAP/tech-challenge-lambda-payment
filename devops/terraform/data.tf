f7885759923cb3dfa36a19d83c9eb98ed82b4d7f
GregoriSimei <gregorisimei@gmail.com>
2025-03-06 19:18:35 -0300

    fix: remove api data source

--- devops/terraform/data.tf
+++ devops/terraform/data.tf
@@ -7,15 +7,6 @@ data "terraform_remote_state" "network" {
   }
 }
 
-data "terraform_remote_state" "app" {
-  backend = "s3"
-  config = {
-    bucket = "tech-challenge-tf-state-bucket"
-    key    = "app/terraform.tfstate"
-    region = "us-west-2"
-  }
-}
-
 data "aws_iam_role" "default" {
   name = "LabRole"
 }
