9f5831f5e75d6628e5935c5e6971c2d131930fa8
GregoriSimei <gregorisimei@gmail.com>
2025-03-24 23:40:43 -0300

    feat: send request to update payment

--- src/usecase/payment/payment.usecase.ts
+++ src/usecase/payment/payment.usecase.ts
@@ -6,7 +6,25 @@ export interface IPaymentUseCase extends UseCase<TPaymentUseCaseRequest, TPaymen
 
 export class PaymentUseCase implements IPaymentUseCase {
     public async execute(request: TPaymentUseCaseRequest): Promise<TPaymentUseCaseResponse> {
-        Logger.info('PaymentUseCase.execute', 'test', process.env.TC_API_URL);
         Logger.info('PaymentUseCase.execute', 'start', request);
+
+        const { orderId, paymentId, status } = request
+        
+        const url = process.env.TC_API_URL + `/order/${orderId}/payment/${paymentId}`;
+        Logger.info('PaymentUseCase.execute', 'test', url);
+
+        try {
+            const response = await axios.get(url, {
+                params: {
+                    status
+                }
+            });
+            Logger.info('PaymentUseCase.execute', 'tech-challenge-api-request', response.data);
+        } catch (error) { 
+            Logger.error('PaymentUseCase.execute', 'error', error);
+            throw error;
+        }
+
+        Logger.info('PaymentUseCase.execute', 'end');
     }
 }
